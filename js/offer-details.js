682efa6d3b2501392555f909639efc7176f8ffa3
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-15 17:11:23 +0100

    feat: YOUAP-83 load the offers data and find the selected offer by id

--- js/offer-details.js
+++ js/offer-details.js
@@ -1,10 +1,23 @@
-
-
-
-function findSelectedId(){
-        const param = new URLSearchParams(window.location.search);
-        const oferId = param.get('id')
-        return oferId;
+function findSelectedId() {
+  const param = new URLSearchParams(window.location.search);
+  const offerId = param.get("id");
+  return offerId;
 }
 
-console.log(findSelectedId());
+function findOffer(offers, offerId) {
+  return offers.find((offer) => offer.id === Number(offerId));
+}
+
+async function loadOffers() {
+  const response = await fetch("../data/offers.json");
+  const offers = await response.json();
+
+  const offerId = findSelectedId();
+  const selectedOffer = findOffer(offers, offerId);
+
+  console.log(selectedOffer);
+}
+
+loadOffers();
+
+// console.log(findSelectedId());
