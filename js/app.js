5c177d1d034a5048a25cfa605a5066e413daff7b
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-18 14:53:04 +0100

    updat: import disply_offers and disply_pagination

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { getData } from "./data.js"
-import { disply_offers } from "./render.js"
+import { disply_pagination , pagnation  } from "./pagination.js";
+import { disply_offers } from "./render.js";
 
 const offers = await getData();
-
-disply_offers(offers)
+disply_offers(pagnation(1,3,offers));
+disply_pagination(offers)
