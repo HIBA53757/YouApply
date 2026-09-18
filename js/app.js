f298fcd86677028e5723bdd35ad30d8ba90d3b5c
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-18 16:44:04 +0100

    docs : update documentation for match the new version

--- js/app.js
+++ js/app.js
@@ -2,9 +2,16 @@ import { getData } from "./data.js"
 import { disply_pagination , pagnation  } from "./pagination.js";
 import { disply_offers } from "./render.js";
 
-const offers = await getData();
-disply_offers(pagnation(1,3,offers));
-disply_pagination(offers)
+const offersList = document.getElementById("Offers_List");
+
+if (offersList) {
+    const offers = await getData();
+    disply_offers(pagnation(1, 3, offers));
+
+    if (document.getElementById("div_contain_number_page")) {
+        disply_pagination(offers);
+    }
+}
 
 
 const mobileMenuBtn = document.getElementById("mobile-menu-btn");
